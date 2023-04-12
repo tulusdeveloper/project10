962846ab23edd39b6fca7660ded93cae1bdbca92
LINUX <linux@localhost.localdomain>
2023-04-13 02:36:51 +0300

    Changes

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = Hash::make('r6r5bb!!');
+        $adminRecords = [
+            ['id'=>1,'name'=>'Admin','type'=>'admin','mobile'=>'+254702782776','email'=>'
+            tulusdeveloper@gmail.com','password'=>'password','image'=>'','status'=>1],
+        ];
+        Admin::insert($adminRecords);
+    }
+}
